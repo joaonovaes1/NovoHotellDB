173309d44e3758dac57f0b034286a5979f2525c6
joaonovaes1 <joaomnovaes1998@gmail.com>
2024-11-15 12:55:18 -0300

    last att

---
+++ src/main/java/com/novohotell/NovoHotellDB/controller/UserController.java
@@ -0,0 +1,16 @@
+//package controller;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UserController {
+//
+//    @PostMapping
+//    public responseEntity<User>createUser(@RequestBody String body){
+//        //
+//        return null;
+//    }
+//
+//}
